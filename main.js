d0b0e9621a84644bbf8a7b0656be2dba05ac830c
Kelsey Bailey <123517600+kelseyb55@users.noreply.github.com>
2023-02-23 10:53:18 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+var menu = {
+    courses: {
+        appetizers: [ ],
+        mains: [ ],
+        desserts: [],
+
+    }
+    addDishToCourse (courseName, dishName, dishPrice) {
+        const dish = {
+          name: dishName,
+          price: dishPrice,
+        };
+
+};
